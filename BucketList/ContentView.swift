7df37094ebf96fbbdc4d1337f062e014147d15e9
Jacek Kosiński <kashiash@gmail.com>
2022-09-19 22:19:41 +0200

    authenticate

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,18 +4,42 @@
 //
 //  Created by Jacek Kosinski U on 17/09/2022.
 //
-
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+                
         }
+        .onAppear(perform: authenticate)
         .padding()
+    }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics
+                                     , error:     &error) {
+            let reason = "We unlock your data i get a bank credit on your name"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                
+                if success {
+                    // authenticate succesfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
